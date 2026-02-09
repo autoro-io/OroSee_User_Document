b54f64b95ce2dea4f75ca2626e78fde357050687
Jaffy-Ou <jaffy.ou@autoro.io>
2026-02-09 14:00:02 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,30 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'OroSee',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'はじめに',
+					autogenerate: { directory: 'introduction' }
+				},
+				{
+					label: 'ガイド',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						//{ label: 　'Example Guide', slug: 'guides/example' },
+						{ label: '2. アカウントと認証', slug: 'guides/account-and-authentication' },
+						{ label: '3. 組織管理', slug: 'guides/3-organizational-management' },
+						{ label: '4. プロジェクト管理', slug: 'guides/4-project-management' },
+						{ label: '5. インタビュー管理', slug: 'guides/5-interview-management' },
+						{ label: '6. 業務管理', slug: 'guides/6-business-management' },
+						{ label: '7. クイックウィン分析', slug: 'guides/7-quick-win-analysis' },
+						{ label: '8. ワークフロー管理', slug: 'guides/8-workflow-management' },
+						{ label: '9. 設定', slug: 'guides/9-settings' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: 'サポート・付録',
 					autogenerate: { directory: 'reference' },
 				},
 			],
---
+++ src/content/docs/index.md
@@ -0,0 +1,6 @@
+---
+title: OroSeeのドキュメント
+description: OroSeeの基本的な使い方について学べる
+---
+
+ようこそOroSeeへ！
